4f88e1b72778e68a5b2987869c3d9e3280e1ba6f
ChristyMim <christy.mims@usm.edu>
2019-10-29 18:31:16 -0500

    Initial commit

---
+++ Used Cars/frmUsedCars.Designer.vb
@@ -0,0 +1,308 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmUsedCars
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmUsedCars))
+        Me.btnPrevious = New System.Windows.Forms.Button()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.txtYear = New System.Windows.Forms.TextBox()
+        Me.txtModel = New System.Windows.Forms.TextBox()
+        Me.txtMake = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnNew = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CloseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveAsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtMileage = New System.Windows.Forms.TextBox()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnPrevious
+        '
+        Me.btnPrevious.Location = New System.Drawing.Point(115, 461)
+        Me.btnPrevious.Name = "btnPrevious"
+        Me.btnPrevious.Size = New System.Drawing.Size(84, 35)
+        Me.btnPrevious.TabIndex = 5
+        Me.btnPrevious.Text = "&Previous"
+        Me.btnPrevious.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(205, 461)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(85, 35)
+        Me.btnNext.TabIndex = 6
+        Me.btnNext.Text = "&Next"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'txtYear
+        '
+        Me.txtYear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtYear.Location = New System.Drawing.Point(224, 337)
+        Me.txtYear.Name = "txtYear"
+        Me.txtYear.Size = New System.Drawing.Size(129, 22)
+        Me.txtYear.TabIndex = 2
+        '
+        'txtModel
+        '
+        Me.txtModel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtModel.Location = New System.Drawing.Point(224, 301)
+        Me.txtModel.Name = "txtModel"
+        Me.txtModel.Size = New System.Drawing.Size(129, 22)
+        Me.txtModel.TabIndex = 1
+        '
+        'txtMake
+        '
+        Me.txtMake.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtMake.Location = New System.Drawing.Point(224, 265)
+        Me.txtMake.Name = "txtMake"
+        Me.txtMake.Size = New System.Drawing.Size(129, 22)
+        Me.txtMake.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(120, 265)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 16)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Make:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(120, 304)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 16)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Model:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(120, 340)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(40, 16)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "Year:"
+        '
+        'btnNew
+        '
+        Me.btnNew.Location = New System.Drawing.Point(296, 461)
+        Me.btnNew.Name = "btnNew"
+        Me.btnNew.Size = New System.Drawing.Size(81, 35)
+        Me.btnNew.TabIndex = 7
+        Me.btnNew.Text = "Ne&w"
+        Me.btnNew.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(476, 24)
+        Me.MenuStrip1.TabIndex = 17
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenToolStripMenuItem, Me.CloseToolStripMenuItem, Me.ToolStripSeparator1, Me.SaveToolStripMenuItem, Me.SaveAsToolStripMenuItem, Me.ToolStripSeparator2, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.OpenToolStripMenuItem.Text = "&Open"
+        '
+        'CloseToolStripMenuItem
+        '
+        Me.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem"
+        Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.CloseToolStripMenuItem.Text = "&Close"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(111, 6)
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.SaveToolStripMenuItem.Text = "&Save"
+        '
+        'SaveAsToolStripMenuItem
+        '
+        Me.SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        Me.SaveAsToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.SaveAsToolStripMenuItem.Text = "Save &As"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(111, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(120, 373)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 16)
+        Me.Label4.TabIndex = 18
+        Me.Label4.Text = "Mileage:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(120, 404)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(42, 16)
+        Me.Label5.TabIndex = 19
+        Me.Label5.Text = "Price:"
+        '
+        'txtMileage
+        '
+        Me.txtMileage.Location = New System.Drawing.Point(224, 373)
+        Me.txtMileage.Name = "txtMileage"
+        Me.txtMileage.Size = New System.Drawing.Size(129, 20)
+        Me.txtMileage.TabIndex = 3
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(224, 403)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(129, 20)
+        Me.txtPrice.TabIndex = 4
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 27)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(438, 162)
+        Me.PictureBox1.TabIndex = 22
+        Me.PictureBox1.TabStop = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Castellar", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(133, 205)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(209, 39)
+        Me.Label6.TabIndex = 23
+        Me.Label6.Text = "Used Cars"
+        '
+        'frmUsedCars
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(476, 559)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.txtMileage)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnNew)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnPrevious)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.txtYear)
+        Me.Controls.Add(Me.txtModel)
+        Me.Controls.Add(Me.txtMake)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmUsedCars"
+        Me.Text = "Used Cars"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnPrevious As Button
+    Friend WithEvents btnNext As Button
+    Friend WithEvents txtYear As TextBox
+    Friend WithEvents txtModel As TextBox
+    Friend WithEvents txtMake As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnNew As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CloseToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtMileage As TextBox
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Used Cars/frmUsedCars.vb
@@ -0,0 +1,139 @@
+﻿Public Class frmUsedCars
+    'Program: Used Cars
+    'Programmer: Christy Mims
+    'Date: 4/26/17
+    'Description: This program allows the user to open, save, close, and display information about cars in the companies
+    'inventory. The program does this through the use of a menu strip, a dialog box, a save file dialog box, buttons, and
+    'text boxes. The user is also able to add and save new cars to the inventory. 
+
+
+    Dim index As Integer = 0
+    Dim cars(), file_name As String
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        Call SaveRecord()
+        If index < cars.Count - 1 Then
+            index += 1
+            Call DisplayRecord()
+        End If
+    End Sub
+
+    Private Sub btnPrevious_Click(sender As Object, e As EventArgs) Handles btnPrevious.Click
+        ' write code to display the previous record
+        Call SaveRecord()
+        If index > 0 Then
+            index = index - 1
+            Call DisplayRecord()
+        End If
+    End Sub
+
+    Private Sub btnNew_Click(sender As Object, e As EventArgs) Handles btnNew.Click
+        Dim new_upper_bound As Integer = cars.GetUpperBound(0) + 1
+        ReDim Preserve cars(new_upper_bound)
+        Call ClearTextBoxes()
+        index = new_upper_bound
+    End Sub
+    Sub DisplayRecord()
+
+        Dim line As String = cars(index)
+        Dim data() As String = line.Split("/"c)
+        txtMake.Text = data(0)
+        txtModel.Text = data(1)
+        txtYear.Text = data(2)
+        txtMileage.Text = data(3)
+        txtPrice.Text = data(4)
+
+    End Sub
+    Sub SaveRecord()
+        Dim data(4) As String
+        data(0) = txtMake.Text
+        data(1) = txtModel.Text
+        data(2) = txtYear.Text
+        data(3) = txtMileage.Text
+        data(4) = txtPrice.Text
+
+        Dim line As String
+        line = Join(data, "/"c)
+        cars(index) = line
+    End Sub
+    Sub ClearTextBoxes()
+        txtMake.Text = ""
+        txtModel.Text = ""
+        txtYear.Text = ""
+        txtMileage.Text = ""
+        txtPrice.Text = ""
+        txtMake.Focus()
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        OpenFileDialog1.ShowDialog()
+        file_name = OpenFileDialog1.FileName
+        cars = IO.File.ReadAllLines(file_name)
+        Call DisplayRecord()
+    End Sub
+
+    Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
+        Call ClearTextBoxes()
+        file_name = ""
+        index = 0
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Call SaveFile(file_name)
+    End Sub
+
+    Private Sub SaveAsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveAsToolStripMenuItem.Click
+        SaveFileDialog1.ShowDialog()
+        Call SaveFile(SaveFileDialog1.FileName)
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Dim response As String = MsgBox("Do you want to save the file before closing the program?", 3, "Save file?")
+        If response = vbNo Then
+            Me.Close()
+        ElseIf response = vbYes Then
+            Call SaveFile(file_name)
+            Me.Close()
+        End If
+    End Sub
+
+    Sub SaveFile(file As String)
+        IO.File.WriteAllLines(file, cars)
+    End Sub
+
+
+
+    ' ************************************ Menu Items *************************
+
+
+
+    ' CODE FOR MENU ITEM "Open" 
+    '    OpenFileDialog1.ShowDialog()
+    '    file_name = OpenFileDialog1.FileName
+    '    contacts = IO.File.ReadAllLines(file_name)
+    '    Call DisplayRecord()
+
+    ' CODE FOR MENU ITEM "Close"
+    '    Call ClearTextBoxes()
+    '    file_name = ""
+    '    index = 0
+
+
+    ' CODE FOR MENU ITEM "Save"
+    '    Call SaveFile(file_name)
+
+
+    ' CODE FOR MENU ITEM "Save As"
+    '    SaveFileDialog1.ShowDialog()
+    '    Call SaveFile(SaveFileDialog1.FileName)
+
+
+    ' CODE FOR MENU ITEM "Exit"
+    '    Dim response As String = MsgBox("Do you want to save the file before closing the program?", 3, "Save file?")
+    '    If response = vbNo Then
+    '        Me.Close()
+    '    ElseIf response = vbYes Then
+    '        Call SaveFile(file_name)
+    '        Me.Close()
+    '    End If
+
+End Class
